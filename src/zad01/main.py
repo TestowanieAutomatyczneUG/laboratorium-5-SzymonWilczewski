c0b69916821ca8d46db07f024e8aff7224b91b08
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-11-02 21:42:45 +0100

    GREEN test_disallow_first_strand_longer

--- src/zad01/main.py
+++ src/zad01/main.py
@@ -1,5 +1,7 @@
 class Hamming:
     def distance(self, str1, str2):
+        if len(str1) > len(str2):
+            raise ValueError("First strand cannot be longer than second!")
         diff = 0
         for i in range(len(str1)):
             if str1[i] != str2[i]:
