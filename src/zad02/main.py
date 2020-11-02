a52c055aa2583bb80e825336ae30ad038ed4a0b8
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-11-02 23:32:24 +0100

    REFACTOR

--- src/zad02/main.py
+++ src/zad02/main.py
@@ -1,9 +1,6 @@
 def roman(num):
-    if num == 575:
-        return "DLXXV"
-
-    numbers = [400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-    symbols = ["CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    numbers = [500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    symbols = ["D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
     i = 0
     roman_number = ""
 
