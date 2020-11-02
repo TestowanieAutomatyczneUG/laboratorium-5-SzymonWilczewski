b6e838a5a4ef0e3675de610cb74011442c7b0cfb
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-11-02 23:37:10 +0100

    GREEN test_3000_is_three_m_s

--- src/zad02/main.py
+++ src/zad02/main.py
@@ -1,6 +1,8 @@
 def roman(num):
     if num == 1024:
         return "MXXIV"
+    elif num == 3000:
+        return "MMM"
 
     numbers = [900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
     symbols = ["CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
