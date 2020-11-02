eb45f148fb52cc1704ba007cc957edb9b9b48d6e
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-11-02 23:14:50 +0100

    REFACTOR

--- src/zad02/main.py
+++ src/zad02/main.py
@@ -1,9 +1,6 @@
 def roman(num):
-    if num == 48:
-        return "XLVIII"
-
-    numbers = [10, 9, 5, 4, 1]
-    symbols = ["X", "IX", "V", "IV", "I"]
+    numbers = [40, 10, 9, 5, 4, 1]
+    symbols = ["XL", "X", "IX", "V", "IV", "I"]
     i = 0
     roman_number = ""
 
