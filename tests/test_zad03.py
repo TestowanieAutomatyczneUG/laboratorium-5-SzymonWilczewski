3d0fd02474c274939bb2a70bf9c50f3ac7030b58
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-11-03 21:56:26 +0100

    RED test_verse_11

--- tests/test_zad03.py
+++ tests/test_zad03.py
@@ -34,7 +34,6 @@ class SongTest(unittest.TestCase):
     def test_verse_10(self):
         self.assertEqual(self.song.verse(10), "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
-    @unittest.skip
     def test_verse_11(self):
         self.assertEqual(self.song.verse(11), "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
 
